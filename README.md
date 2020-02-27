4ac0f2829c2c833221f53c52bcdc0e5683279cbc
MingYan <mingyan24@126.com>
2020-02-27 15:16:56 +0800

    add script

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ```
 python get_mVISTA_annotation_file_from_genbank_1.py -i input_genbank.gb
 ```
+### download_gb_or_fa_from_NCBI_cp_genome_database_1.py
+
+> NCBI批量下载序列,需要填写自己的邮箱
+- 使用方法
+```
+python download_gb_or_fa_from_NCBI_cp_genome_database_1 -f gb/fasta -a assession.number
+```
---
+++ download_gb_or_fa_from_NCBI_cp_genome_database_1.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import argparse
+parser = argparse.ArgumentParser(description='This script was used to download gb or fasta file of cp genome from NCBI nucleotides database')
+parser.add_argument('-f','--format',help='Please input file format you want to download',required=True)
+parser.add_argument('-a','--accession',help='file name contain accession number of cp genome you want to download',required=True)
+args = parser.parse_args()
+
+
+fr = open(args.accession,'r')
+acc = []
+for line in fr:
+	acc.append(line.strip())
+print("The number of sequence will be downloaded is: ",len(acc))
+
+from Bio import Entrez
+from Bio import SeqIO
+Entrez.email = "添加自己的邮箱"
+
+fold_name = "downloaded_seq_"+args.format
+os.mkdir(fold_name)
+os.chdir(fold_name)
+
+for line in acc:
+	print(line + " will be downloaded!")
+	fw = open(line+"."+args.format,'w')
+	cp = Entrez.efetch(db='nucleotide',id=[line],rettype=args.format)
+	seq = SeqIO.read(cp,args.format)
+	SeqIO.write(seq,fw,args.format)
+	fw.close()
+	print(line + " is downloaded!")
+
+print(len(os.listdir()),"sequence have been downloaded")
+
+
+
