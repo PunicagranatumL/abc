a5071a4a55472eae8909cc9623a5fbec7f2c70ad
MingYan <mingyan24@126.com>
2020-05-02 09:41:09 +0800

    修改注释起始位置错误的问题

--- get_mVISTA_annotation_file_from_genbank_1.py
+++ get_mVISTA_annotation_file_from_genbank_1.py
@@ -13,28 +13,28 @@ for rec in SeqIO.parse(args.genbank,"gb"):
 		if feature.type == "gene":
 			for part in feature.location.parts:
 				if part.strand == -1:
-					start_location = str(part.start)
+					start_location = str(part.start+1)
 					end_location = str(part.end)
 					gene_name = feature.qualifiers["gene"][0]
 					fw.write("<\t%s\t%s\t%s\n"%(start_location,end_location,gene_name))
 				else:
-					start_location = str(part.start)
+					start_location = str(part.start+1)
 					end_location = str(part.end)
 					gene_name = feature.qualifiers["gene"][0]
 					fw.write(">\t%s\t%s\t%s\n"%(start_location,end_location,gene_name))
 		elif feature.type == "CDS":
 			for part in feature.location.parts:
 				if part.strand == -1:
-					start_location = str(part.start)
+					start_location = str(part.start+1)
 					end_location = str(part.end)
 					fw.write("%s\t%s\t%s\n"%(start_location,end_location,"exon"))
 				else:
-					start_location = str(part.start)
+					start_location = str(part.start+1)
 					end_location = str(part.end)
 					fw.write("%s\t%s\t%s\n"%(start_location,end_location,"exon"))
 		elif feature.type == "tRNA" or feature.type == "rRNA":
 			for part in feature.location.parts:
-				start_location = str(part.start)
+				start_location = str(part.start+1)
 				end_location = str(part.end)
 				fw.write("%s\t%s\t%s\n"%(start_location,end_location,"utr"))
 		else:
